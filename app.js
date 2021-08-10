258d504c77d728e3148d1c70a13c0ebfd23f54a2
Raoaditi <aditi.angad18@siesgst.ac.in>
2021-08-10 17:29:15 +0530

    weather updated

--- app.js
+++ app.js
@@ -9,17 +9,22 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+    // res.render("index");
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/', (req, res) => {
-    const query = req.body.cityName;
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    const query = capitalizeFirstLetter(req.body.cityName);
     const apiKey = "bf779ed7679ca2810d5944e197b9f8fd";
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=metric";
     https.get(url, (response => {
         response.on('data', (d) => {
             const myData = JSON.parse(d);
-            const weather = myData.weather[0].description;
+            const weather = capitalizeFirstLetter(myData.weather[0].description);
             const icon = myData.weather[0].icon;
             const iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             const temp = myData.main.temp;
