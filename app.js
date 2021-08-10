d44f398077e9d4f1b8c5ee8cafe6abc904540910
Raoaditi <aditi.angad18@siesgst.ac.in>
2021-08-10 13:04:51 +0530

    Added logo in index

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//weather api key : bf779ed7679ca2810d5944e197b9f8fd
 const express = require('express');
 const path = require('path');
 const https = require('https');
@@ -6,11 +5,17 @@ const https = require('https');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+<<<<<<< HEAD
+app.use(express.static('public'));
+=======
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+>>>>>>> 3ebca078fb39a5d207f470a817591dfd43ab093a
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.render('index');
 });
+
 app.post('/', (req, res) => {
     const query = req.body.cityName;
     const apiKey = "bf779ed7679ca2810d5944e197b9f8fd";
@@ -19,17 +24,15 @@ app.post('/', (req, res) => {
         response.on('data', (d) => {
             const myData = JSON.parse(d);
             const weather = myData.weather[0].description;
-            //const icon = myData.weather[0].icon;
-            //const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            const icon = myData.weather[0].icon;
+            const iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             const temp = myData.main.temp;
-            res.write("<h1>" + query + "</h1>");
-            res.write("<h4> Weather: " + weather + "</h4>");
-            //res.write("<img src = " + imageURL + ">");
-            res.write("<h4> Temperature: " + temp + "</h4>");
+            res.render('weather', {city : query, weather, iconURL, temp})
             res.send();
         });
     }));
 });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT} ... `);
